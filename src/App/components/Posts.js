26d71dcea11f90fd4f48d587e02fab6fe3b3e88f
Jamie Youngman <JamieYoungman@jamies-mbp.home>
2019-02-08 16:24:55 +0000

    added a redux store to create-react-app

--- src/App/components/Posts.js
+++ src/App/components/Posts.js
@@ -1,25 +1,21 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { func, array, object } from 'prop-types';
+import { fetchPosts } from '../actions/postActions';
 
 class Posts extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			posts: []
-		};
-	}
 	componentDidMount() {
-		fetch('https://jsonplaceholder.typicode.com/posts')
-			.then(res => res.json())
-			.then(data =>
-				this.setState({
-					posts: data
-				})
-			);
+		this.props.fetchPosts();
 	}
-	render() {
-		const { posts } = this.state;
 
-		const postItems = posts.map(post => (
+	componentWillReceiveProps(nextProps) {
+		if (nextProps.newPost) {
+			this.props.posts.unshift(nextProps.newPost);
+		}
+	}
+
+	render() {
+		const postItems = this.props.posts.map(post => (
 			<div key={post.id}>
 				<h3>{post.title}</h3>
 				<p>{post.body}</p>
@@ -34,4 +30,18 @@ class Posts extends React.Component {
 	}
 }
 
-export default Posts;
+Posts.propTypes = {
+	fetchPosts: func.isRequired,
+	posts: array.isRequired,
+	newPost: object
+};
+
+const mapStateToProps = state => ({
+	posts: state.posts.items,
+	newPost: state.posts.item
+});
+
+export default connect(
+	mapStateToProps,
+	{ fetchPosts }
+)(Posts);
